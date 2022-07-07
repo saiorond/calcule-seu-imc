1f3ca8cf121b74b305c70a6406c26038de40a218
Débora <d.saioron@hotmail.com>
2022-07-06 21:51:56 -0300

    adicionando funções

--- calcula-imc.js
+++ calcula-imc.js
@@ -1,4 +1,12 @@
-const nome = document.getElementById('#nome');
-const altura = document.getElementById('#altura');
-const peso = document.getElementById('#peso');
+const btn = document.querySelector("#enviar");
 
+btn.addEventListener("click", function(e) {
+
+    e.preventDefault();
+
+    const nome = document.querySelector("#nome");
+
+    const value = nome.value;
+
+    console.log(value)
+});
