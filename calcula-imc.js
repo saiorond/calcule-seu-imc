b8b4ebde50e4ef9430a53a0c26c33a06c55e7a39
Débora <d.saioron@hotmail.com>
2022-07-09 14:17:26 -0300

    mudança no js

--- calcula-imc.js
+++ calcula-imc.js
@@ -18,6 +18,6 @@ function calculaImc() {
   let peso = document.getElementById("peso").value;
   let altura = document.getElementById("altura").value;
   let imc = peso / (altura * altura);
-  document.getElementById("resultado").innerHTML = imc;
+  document.getElementById("resultado").innerHTML = `${nome}, seu ${imc} é: `;
 }
 
